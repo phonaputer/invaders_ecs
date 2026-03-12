f445a023bf8a9b255988cba2dd964c667f24081c
Phonaputer <njerome@uwalumni.com>
2026-03-12 16:03:49 +0900

    Create SDL3 project skeleton.

---
+++ src/main.cpp
@@ -0,0 +1,22 @@
+#define SDL_MAIN_USE_CALLBACKS
+
+#include <SDL3/SDL.h>
+#include <SDL3/SDL_main.h>
+
+SDL_AppResult SDL_AppInit([[maybe_unused]] void **appstate, [[maybe_unused]] int argc, [[maybe_unused]] char *argv[]) {
+  SDL_Log("Setup complete...");
+
+  return SDL_APP_CONTINUE;
+}
+
+SDL_AppResult SDL_AppEvent([[maybe_unused]] void *appstate, [[maybe_unused]] SDL_Event *event) {
+  return SDL_APP_CONTINUE;
+}
+
+SDL_AppResult SDL_AppIterate([[maybe_unused]] void *appstate) {
+  return SDL_APP_SUCCESS;
+}
+
+void SDL_AppQuit([[maybe_unused]] void *appstate, [[maybe_unused]] SDL_AppResult result) {
+  SDL_Log("Quitting...");
+}
